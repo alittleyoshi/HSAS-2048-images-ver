17db981bc526ab7c38f140c8a9f794173bd52ef3
alittleyoshi <77890872+alittleyoshi@users.noreply.github.com>
2024-06-04 16:48:17 +0800

    修改生成数字 难度增加

--- js/game_manager.js
+++ js/game_manager.js
@@ -77,7 +77,7 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = Math.random() < 0.9 ? 2 : 2;//TODO ADD A SETTING
+    var value = Math.random() < 0.9 ? 2 : 4;//TODO ADD A SETTING
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
